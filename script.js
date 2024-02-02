1ac86a5811f456bd8c10be1d7b4aa49aef035b85
Coder <kiryanov001@yandex.ru>
2024-02-02 23:22:01 +0300

    Код для hw2

---
+++ script.js
@@ -0,0 +1,63 @@
+//n1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+//n2
+let firstPhone = 2007;
+alert(firstPhone);
+//n3
+let createJS = 'brandonEich';
+alert(createJS);
+//n4
+let first = 10;
+let second = 2;
+alert(first + second);
+alert(first - second);
+alert(first * second);
+alert(first / second);
+//n5
+let f = 2;
+let s = 5;
+let result = 2 ** 5;
+alert(result);
+//n6
+let nine = 9;
+let two = 2;
+alert(nine % two);
+//n7
+let num = 1;
+num = num += 5;
+num = num -= 3;
+num = num *= 7;
+num = num /= 3;
+num = ++num;
+num = --num;
+alert(num);
+
+//n8
+let age = prompt('Сколько вам лет?');
+alert(age);
+//n9.0
+let user = {
+    name: 'Vlad',
+    age: 26,
+    isAdmin: false,
+}
+//n9.1
+user['city of residence'] = 'Pyatigorsk';
+//n9.2
+user.age = 27;
+//n9.3
+delete user['city of residence'];
+//n9.4
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user.info);
+//n10
+let yourName = prompt('Введите Ваше имя');
+alert(`Привет, ${yourName} !`);
+
+
+
+
+
